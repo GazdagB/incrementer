ee27195cc621b9b3126ae5ff77856ef061fb60b8
theCodeDesigner <gazdagbal@gmail.com>
2023-11-09 22:50:54 +0100

    Random backgournd generator on first load.

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,28 @@ import BigStep from './components/BigStep/BigStep';
 import React from 'react';
 import { useState } from 'react';
 import StartingMessage from './components/StartingMessage/StartingMessage';
+import { useEffect } from 'react';
 
 function App() {
 
   const [count, setCount] = useState(0);
+  const [bgColor, setBgColor] = useState(null);
+
+
+  useEffect(() => {
+    function getRandomPastelColor() {
+      const hue = Math.floor(Math.random() * 360);
+      const pastel = 'hsl(' + hue + ', 60%, 90%)';
+      return pastel;
+    }
+
+    setBgColor(getRandomPastelColor());
+  }, []);
+
+  
   
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor: bgColor}}>
       <Incrementer count={count} setCount={setCount} />
 
       {count === 0 && <StartingMessage />}
@@ -20,5 +35,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
