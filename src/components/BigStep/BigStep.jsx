4a30360b65063094c279347d9758d3d06cb59606
theCodeDesigner <gazdagbal@gmail.com>
2023-11-09 22:26:28 +0100

    1. Increment DONE in BigStep.jsx

--- src/components/BigStep/BigStep.jsx
+++ src/components/BigStep/BigStep.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
-
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
+import "./BigStep.css";
 
 const BigStep = () => {
   return (
-    <div>
-      <h1>BigStep</h1>
-      <FontAwesomeIcon icon={faQuoteLeft} />
-      <img src="https://thatgift.files.wordpress.com/2015/05/armstrong.jpg" alt="" />
+    <div className="BigStep">
+      <div className="d-flex flex-column justify-content-center align-items-center frame text-light">
+        <FontAwesomeIcon className="quote" icon={faQuoteLeft} />
+        <p className="quote lead fw-bold">
+          <span>One</span> small step for a man, <span>one</span> giant leap for
+          mankind
+        </p>
+        <p className="author">-Neil Armstrong</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BigStep
+export default BigStep;
