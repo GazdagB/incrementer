f0639700318d6ff9fb86d8725c36d5a7baacc79d
theCodeDesigner <gazdagbal@gmail.com>
2023-11-09 22:53:58 +0100

    Deleted pastel generator.

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,17 @@ import BigStep from './components/BigStep/BigStep';
 import React from 'react';
 import { useState } from 'react';
 import StartingMessage from './components/StartingMessage/StartingMessage';
-import { useEffect } from 'react';
+
 
 function App() {
 
   const [count, setCount] = useState(0);
-  const [bgColor, setBgColor] = useState(null);
-
-
-  useEffect(() => {
-    function getRandomPastelColor() {
-      const hue = Math.floor(Math.random() * 360);
-      const pastel = 'hsl(' + hue + ', 60%, 90%)';
-      return pastel;
-    }
-
-    setBgColor(getRandomPastelColor());
-  }, []);
+  
 
   
   
   return (
-    <div className="App" style={{backgroundColor: bgColor}}>
+    <div className="App" style={{backgroundColor: "#fffae9"}}>
       <Incrementer count={count} setCount={setCount} />
 
       {count === 0 && <StartingMessage />}
