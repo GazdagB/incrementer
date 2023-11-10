99931508b878669fa03d845f299aa917c8fe12c6
theCodeDesigner <gazdagbal@gmail.com>
2023-11-10 23:07:11 +0100

    Modal added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 import StartingMessage from "./components/StartingMessage/StartingMessage";
 import Message from "./components/Message/Message";
 import { useEffect } from "react";
+import { Modal } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 function App() {
   //CHANGE THIS TO CHANGE THE START AND END OF THE COUNTER
@@ -15,6 +17,11 @@ function App() {
   const [count, setCount] = useState(0);
   const [isUpDisabled, setIsUpDisabled] = useState(false);
   const [isDownDisabled, setIsDownDisabled] = useState(false);
+  const [show,setShow] = useState(false)
+
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
     function isReached() {
@@ -35,6 +42,24 @@ function App() {
 
   return (
     <div className="App" style={{ backgroundColor: "#fffae9" }}>
+
+  
+
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleClose}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+
       <Incrementer
         isUpDisabled={isUpDisabled}
         isDownDisabled={isDownDisabled}
@@ -46,11 +71,14 @@ function App() {
         <Message
           title={"Woooow!🎉 You are persistent!"}
           subtext={"We didn't come up with anything for this number yet!💻"}
+          isButton={true}
+          btntext={"Suggest Ideas!"}
+          btnOnClick={handleShow}
         />
       )}
       {count === 0 && <StartingMessage />}
       {count === 1 && <BigStep />}
-      
+
       {count >= end && (
         <Message
           title={"Woooow!🎉 You are persistent!"}
--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Message.css'
+import { Button } from 'react-bootstrap'
 
-const Message = ({title,subtext}) => {
+const Message = ({title,subtext,btntext,isButton,btnOnClick}) => {
   return (
     <div className='Message'>
       <p className='display-5 text-center'>{title}</p>
       <p className="lead">{subtext}</p>
+      {isButton &&   <Button variant="primary" onClick={btnOnClick}>
+      {btntext}
+    </Button> }
     </div>
   )
 }
